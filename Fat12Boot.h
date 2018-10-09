52f8ca643242c20dcf189f4717aa27495622266b
matthew meadwell <mmeadwell22@gmail.com>
2018-10-09 17:37:26 -0400

    adding new files and starting header files

---
+++ Fat12Boot.h
@@ -0,0 +1,18 @@
+#ifndef FAT12BOOT_H
+#define FAT12BOOT_H
+
+struct FatBoot{
+    unsigned int BYTES_PER_SECTOR;
+    unsigned int SECTORS_PER_CLUSTER;
+    unsigned int RESERVED_SECTORS;
+    unsigned int NUMBER_OF_FATS;
+    unsigned int MAX_ROOT_DIRS;
+    unsigned int TOTAL_SECTORS;
+    unsigned int SECTORS_PER_FAT;
+    unsigned int SECTOR_PER_TRACK;
+    unsigned int NUMBER_OF_HEADS;
+    unsigned int VOLUME_ID;
+    unsigned int VOLUME_LABEL;
+};
+
+#endif
